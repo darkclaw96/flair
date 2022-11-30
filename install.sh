2eb08ec974e40857bc24a0ed8aba5f5a308012d6
darkclaw96 <89162705+darkclaw96@users.noreply.github.com>
2022-11-30 16:28:58 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Installing Essential Programs 
+apt install bspwm sxhkd kitty rofi polybar picom pcmanfm nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pavucontrol vlc nala exa neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji -y
+
+# Install brave-browser
+git clone https://github.com/djoma98/debian-brave
+cd debian-brave
+chmod +x install.sh
+./install.sh
+cd $builddir
+rm -rf debian-brave
